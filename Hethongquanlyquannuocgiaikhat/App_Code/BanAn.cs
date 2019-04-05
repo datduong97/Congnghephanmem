6b896f34e5e5a606d4016a8f568027197a12e6ad
TuanNguyenk56 <48193461+TuanNguyenk56@users.noreply.github.com>
2019-04-05 16:40:42 +0700

    Add files via upload

---
+++ Hethongquanlyquannuocgiaikhat/App_Code/BanAn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class BanAn
+{
+	public int MaBan { get; set; }
+    public string TenBan { get; set; }         
+    public string TrangThai { get; set; }
+    public BanAn()
+    {
+
+    }
+    public BanAn(string tenBan, string trangThai)
+    {        
+        TenBan = tenBan;
+        TrangThai = trangThai;
+    }
+    public BanAn(int maBan, string tenBan, string trangThai)
+    {
+        MaBan = maBan;
+        TenBan = tenBan;        
+        TrangThai = trangThai;
+    }
+}
---
+++ Hethongquanlyquannuocgiaikhat/App_Code/XuLyNguoiDung.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+public class XuLyNguoiDung
+{
+    DuLieu dl = new DuLieu();
+    public DataTable ListNguoiDung()
+    {
+        SqlParameter[] sqlParams = { };
+        return dl.Select("SELECT * FROM Account", sqlParams);
+    }
+    public DataTable ListNguoiDungActive()
+    {
+        SqlParameter[] sqlParams = { };
+        return dl.Select("SELECT * FROM Account WHERE Type = 1", sqlParams);
+    }
+    public DataTable GetNguoiDung(int id)
+    {
+        SqlParameter idnd = new SqlParameter("@UserName", id);
+        SqlParameter[] sqlParams = { idnd };
+        return dl.Select("SELECT * FROM Account WHERE UserName = @UserName", sqlParams);
+    }
+    public DataTable CheckDangNhap(NguoiDung obj)
+    {
+        SqlParameter tdn = new SqlParameter("@UserName", obj.TenDangNhap);
+        SqlParameter mk = new SqlParameter("@Password", obj.MatKhau);
+        SqlParameter[] sqlParams = { tdn, mk };
+        return dl.Select("SELECT * FROM Account WHERE UserName = @UserName AND Password =@Password AND Type=1", sqlParams);
+    }
+    public DataTable CheckDangNhapAdmin(NguoiDung obj)
+    {
+        SqlParameter tdn = new SqlParameter("@UserName", obj.TenDangNhap);
+        SqlParameter mk = new SqlParameter("@Password", obj.MatKhau);
+        SqlParameter[] sqlParams = { tdn, mk };
+        return dl.Select("SELECT * FROM Account WHERE UserName = @UserName AND Password =@Password AND Type=1 ", sqlParams);
+    }
+    public int SuaNguoiDung(int id)
+    {
+        SqlParameter idnd = new SqlParameter("@UserName", id);
+        SqlParameter[] sqlParams = { idnd };
+        return dl.SaveChanges("DELETE FROM Account WHERE UserName = @UserName", sqlParams);
+    }
+    public int XoaNguoiDung(int id)
+    {
+        SqlParameter idnd = new SqlParameter("@UserName", id);
+        SqlParameter[] sqlParams = { idnd };
+        return dl.SaveChanges("DELETE FROM Account WHERE UserName = @UserName", sqlParams);
+    }
+}
