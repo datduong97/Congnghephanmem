9415d4daaf0056c7ccdfbf4adbd5330d31d3b12a
datdv260497@gmail.com <datdv260497@gmail.com>
2019-05-02 21:22:26 +0700

    up
    menu

---
+++ Dương Văn Đạt/Web.Customer/Web.Customer/Data/WebDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Customer.Data
+{
+    public  class WebDbContext:DbContext
+    {
+        public WebDbContext (DbContextOptions<WebDbContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Table> Tables { get; set; }
+        public virtual DbSet<Food> Foods { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Table>(model =>
+            {
+                model.ToTable("Table","dbo");
+            });
+            modelBuilder.Entity<Food>(model =>
+            {
+                model.ToTable("Food", "dbo");
+            });
+            modelBuilder.Entity<FoodCategory>(model =>
+            {
+                model.ToTable("FoodCategory", "dbo");
+            });
+            modelBuilder.Entity<DrinkCategory>(model =>
+            {
+                model.ToTable("DrinkCategory", "dbo");
+            });
+            modelBuilder.Entity<Drink>(model =>
+            {
+                model.ToTable("Drink", "dbo");
+            });
+        }
+    }
+}
