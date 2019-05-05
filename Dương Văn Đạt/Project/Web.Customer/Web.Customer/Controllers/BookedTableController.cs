67002f349dc331f1a0475b46faaa3ce9afedbacf
datdv260497@gmail.com <datdv260497@gmail.com>
2019-05-05 21:33:51 +0700

    up

---
+++ Dương Văn Đạt/Project/Web.Customer/Web.Customer/Controllers/BookedTableController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Customer.Data;
+using Web.Customer.Models;
+using Web.Customer.Services;
+
+namespace Web.Customer.Controllers
+{
+    public class BookedTableController: Controller
+    {
+        ITableManager _tablemanager;
+        IBookedTableManager _manager;
+        public BookedTableController(IBookedTableManager manager,ITableManager tableManager)
+        {
+            _manager = manager;
+            _tablemanager = tableManager;
+        }
+
+        public virtual async Task<IActionResult> Create()
+        {
+            var rs = await _tablemanager.GetTablesAsync();
+            ViewBag.table = rs; ;
+            var retail = new BookedTableEditViewModel();
+            return View(retail);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(BookedTable bookedTable)
+        {
+            int idTable = bookedTable.idTable;
+            string CustomerPhone = bookedTable.CustomerPhone;
+            string CustomerName = bookedTable.CustomerName;
+            string CustomerAddress = bookedTable.CustomerAddress;
+            string BookDate = bookedTable.BookDate;
+            string BookTimeStart = bookedTable.BookTimeStart;
+            string BookTimeEnd = bookedTable.BookTimeEnd;
+
+            var entity = await _manager.AddAsync(new BookedTable
+            {
+                idTable=idTable,
+                CustomerAddress = CustomerAddress,
+                CustomerName = CustomerName,
+                CustomerPhone = CustomerPhone,
+                BookDate = BookDate,
+                BookTimeStart= BookTimeStart,
+                BookTimeEnd= BookTimeEnd
+            });
+            return Json(entity);
+        }
+
+
+
+    }
+}
---
+++ Dương Văn Đạt/Project/Web.Customer/Web.Customer/Models/BookedTableEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Customer.Models
+{
+    public class BookedTableEditViewModel
+    {
+        public int idTable { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerAddress { get; set; }
+        public string BookDate { get; set; }
+        public string BookTimeStart { get; set; }
+        public string BookTimeEnd { get; set; }
+    }
+}
